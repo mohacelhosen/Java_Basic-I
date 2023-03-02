76cd60ca9001c839abdc07d67605f5f256bd3f7e
Mohacel <118649370+mohacelhosen@users.noreply.github.com>
2023-03-02 23:37:53 +0600

    Create Q4_Generate_Day.java

---
+++ Q4_Generate_Day.java
@@ -0,0 +1,30 @@
+/*
+Write a Java program that keeps a number from the user and generates an integer between 1 and 7 and displays the name of the weekday. 
+*/
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter number (1-7): ");
+		int day=in.nextInt();
+		switch(day){
+			case 1: System.out.print("Sunday");
+						break;
+			case 2: System.out.print("Monday");
+						break;
+			case 3: System.out.print("Tuesday");
+						break;
+			case 4: System.out.print("Wednesday");
+						break;
+			case 5: System.out.print("Thursday");
+						break;
+			case 6: System.out.print("Friday");
+						break;
+			case 7: System.out.print("Saturday");
+						break;
+			default:
+				System.out.print("Invalid input");
+			
+		}
+	}
+}
