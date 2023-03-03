f41b87675e5772a50a3013085daec77cc81e234f
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-03 20:35:53 +0600

    triangle with a number

---
+++ Q13_pattern.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/*
+    Write a program in Java to display the pattern like right angle triangle with a number.
+
+Test Data
+Input number of rows : 10
+Expected Output :
+
+1                                                                                
+12                                                                               
+123                                                                              
+1234                                                                             
+12345                                                                            
+123456                                                                           
+1234567                                                                          
+12345678                                                                         
+123456789                                                                        
+12345678910
+ */
+
+public class Q13_pattern {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(""+j);
+            }
+            System.out.println();
+        }
+    }
+}
